ff1593963aa2a2e8b305a5707b16db3f2a47908f
MEMKARAGOZ <130648118+MEMKARAGOZ@users.noreply.github.com>
2023-05-17 02:52:45 +0300

    core java while loop

---
+++ WhileLoop.java
@@ -0,0 +1,32 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println("while loop 0<==>10");
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("while loop 0<==>9");
+        int x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x++;
+        }
+        System.out.println("while loop 0<==>10 çift sayılar");
+        int y = 0;
+        while (y <= 10) {
+            System.out.println(y);
+            y+=2;
+        }
+        System.out.println("while loop 0<==>10 tek sayılar");
+        int z = 1;
+        while (z <= 10) {
+            System.out.println(z);
+            z+=2;
+        }
+
+        System.out.println("While loops bitti");
+
+    }
+}
